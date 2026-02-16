3a856307957899cabd74ac1f2744fdecdda84fe0
developer-shahidul <developer-shahidul@outlook.com>
2026-02-16 15:36:00 +0600

    save local changes before switch

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,12 +1,15 @@
 import { Suspense, use } from "react";
 import { AuthContext } from "../../Contexts/AuthContexts/AuthContext";
-import MyPostedJobList from "./myPostedJobList";
-import { jobsCreatedByPromise } from "../../Api/JobApi";
+import MyPostedJobList from "./MyPostedJobList";
+// import { jobsCreatedByPromise } from "../../Api/JobApi";
+import UseJobApi from "../../Api/UseJobApi";
 
 const MyPostedJobs = () => {
   const { user } = use(AuthContext);
+
   // console.log("firebase token", user.accessToken);
 
+  const { jobsCreatedByPromise } = UseJobApi();
   return (
     <div>
       <h1>My posted Jobs</h1>
